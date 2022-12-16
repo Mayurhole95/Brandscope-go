2e6360216bdb537c32a3339b78df817d37188637
mukul8301 <mukul.chaudhari@joshsoftware.com>
2022-12-16 11:00:31 +0530

    Removed comments

--- db/domain.go
+++ db/domain.go
@@ -18,8 +18,3 @@ type LogID struct {
 	ReleaseID              string `db:"release_id`
 	BrandID                string `db:"brand_id"`
 }
-
-// INSERT INTO public.release_uploads(
-// id, file_name, no_of_products, no_of_product_releases, no_of_size_breaks, status, error_file, original_file_location, message,release_id, brand_id,  created_at, updated_at)
-// VALUES (700, "Testing", 7, 7, 3, 'pending', 'Dash_Summer 21_20221201121220_errors.csv', 'Dash_Summer 21_20221201121220.csv', 'File Validation',  206, 76,  ?, ?);
-// 2018-11-30 02:31:35.28
