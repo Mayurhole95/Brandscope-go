ca2f770998ebfd1b2cc035e9f4b842faf1fe2c4f
mukul8301 <mukul.chaudhari@joshsoftware.com>
2022-12-16 11:00:03 +0530

    Add Log id struct

--- db/domain.go
+++ db/domain.go
@@ -12,3 +12,14 @@ type Verify struct {
 	SKU         string `db:"sku"`
 	Colour_code string `db:"colour_code"`
 }
+
+type LogID struct {
+	Original_file_location string `db:"original_file_location"`
+	ReleaseID              string `db:"release_id`
+	BrandID                string `db:"brand_id"`
+}
+
+// INSERT INTO public.release_uploads(
+// id, file_name, no_of_products, no_of_product_releases, no_of_size_breaks, status, error_file, original_file_location, message,release_id, brand_id,  created_at, updated_at)
+// VALUES (700, "Testing", 7, 7, 3, 'pending', 'Dash_Summer 21_20221201121220_errors.csv', 'Dash_Summer 21_20221201121220.csv', 'File Validation',  206, 76,  ?, ?);
+// 2018-11-30 02:31:35.28
